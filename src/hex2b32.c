beba107372a4fcdd2aeabc9abcde98bc3a4a955c
RedRoosterKey <f11f4796@opayq.com>
2015-06-14 00:05:10 -0500

    Update to use GPL v 3

--- src/hex2b32.c
+++ src/hex2b32.c
@@ -5,18 +5,21 @@
  *
  * @section LICENSE
  *
- * GNU GENERAL PUBLIC LICENSE, Version 2, June 1991
+ *                    GNU GENERAL PUBLIC LICENSE
+ *                     Version 3, 29 June 2007
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
+ *    hex2b32 is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * https://www.gnu.org/copyleft/gpl.html
+ *    hex2b32 is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @section DESCRIPTION
  *
